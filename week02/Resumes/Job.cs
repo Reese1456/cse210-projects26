97aec40ab0472eab0458a55c18b0199667069c1e
Reese1456 <reese.blignaut@gmail.com>
2026-01-22 09:11:07 +0100

    Finished Resume Assignment

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,23 @@
+using System;
+/*
+Job
+
+_company : string
+_jobTitle : string
+_startYear : int
+_endYear : int
+
+Display() : void
+*/
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,27 @@
+using System;
+
+/*
+Resume
+
+_name : string
+_jobs : List<Job>
+
+Display() : void
+*/
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
